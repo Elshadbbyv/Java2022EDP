6336557f74ec386c9cc8fc3f797d6562f411b029
elshadbbyv <babayevelshad09@gmail.com>
2022-09-24 16:40:08 +0400

    Initial commit: Encapsulation, getter and settor method in private references add Test.html files and read output

--- src/main/java/az/tutorial/engindemir/bootcamp/oop/Main.java
+++ src/main/java/az/tutorial/engindemir/bootcamp/oop/Main.java
@@ -3,33 +3,48 @@ package az.tutorial.engindemir.bootcamp.oop;
 public class Main {
     public static void main(String[] args) {
         Product product1 = new Product();
-        product1.name = "Delongi Kahve Makinasi";
-        product1.unitPrice = 7500;
-        product1.discount= 7;
-        product1.imageUrl= "image.svg";
-        product1.unitsInStock= 3;
+        product1.setName("Delongi Kahve Makinasi") ;
+        product1.setUnitPrice( 7500);
+        product1.setDiscount(7);
+        product1.setImageUrl("image.svg");
+        product1.setUnitsInStock(3);
 
         Product product2 = new Product();
-        product2.name = "Delongi Kahve Makinasi";
-        product2.unitPrice = 7500;
-        product2.discount= 7;
-        product2.imageUrl= "image.svg";
-        product2.unitsInStock= 3;
+        product2.setName("Sweg Kahve Makinasi") ;
+        product2.setUnitPrice( 7500);
+        product2.setDiscount(7);
+        product2.setImageUrl("image.svg");
+        product2.setUnitsInStock(3);
 
         Product product3 = new Product();
-        product3.name = "Delongi Kahve Makinasi";
-        product3.unitPrice = 7500;
-        product3.discount= 7;
-        product3.imageUrl= "image.svg";
-        product3.unitsInStock= 3;
+        product3.setName("Delongi Kahve Makinasi") ;
+        product3.setUnitPrice( 7500);
+        product3.setDiscount(7);
+        product3.setImageUrl("image.svg");
+        product3.setUnitsInStock(3);
 
         Product[] product = {product1, product2, product3};
         System.out.println("<ul>");
         for (Product products: product) {
-            System.out.println(""+products);
+            System.out.println("<li>"+products.getName()+"</li>");
 
         }
+        System.out.println("</ul>");
 
+        IndividualCustomer individualCustomer = new IndividualCustomer();
+        individualCustomer.setId(1);
+        individualCustomer.setCustomerNumber("00001");
+        individualCustomer.setPhone("055536273");
+        individualCustomer.setFirstName("Elshad");
+        individualCustomer.setLastName("Babayev");
 
+        CorporateCustomer corporateCustomer = new CorporateCustomer();
+        corporateCustomer.setId(2);
+        corporateCustomer.setCustomerNumber("00002");
+        corporateCustomer.setPhone("0556328282");
+        corporateCustomer.setCompanyName("Java");
+        corporateCustomer.setTaxNumber("74937");
+
+        Customer[] customers = {individualCustomer,corporateCustomer};
     }
 }
